fd7e4531f437e9869746b024759cf5655a6d0e16
dasom037 <dasom037@gmail.com>
2022-03-22 22:00:36 +0900

    Add getters, mapGetters, mapState

--- src/components/Users/AllUsers.vue
+++ src/components/Users/AllUsers.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h1>All Users</h1>
+    <h1>All Users({{ count }})</h1>
+    <h3>{{ percent }} %</h3>
     <v-list two-line>
       <v-list-tile 
-        v-for="(user, index) in allUsers"
+        v-for="(user, index) in allUsers" // mapState로 대체 $store.state.allUsers
         :key="index"
         avatar
       >
@@ -23,20 +24,33 @@
 
 <script>
 import { EventBus } from '@/main.js'
+import { mapState, mapGetters } from 'vuex'
 
   export default {
     data() {
       return {
-        allUsers:[
-          {userId: 'hoza123', password: '123', name: 'Hoza', address: 'Seoul', src:'https://goo.gl/oqLfJR'},
-          {userId: 'max123', password: '456', name: 'Max', address: 'Berlin', src:'https://goo.gl/Ksk9B9'},
-          {userId: 'lego123', password: '789', name: 'Lego', address: 'Busan', src:'https://goo.gl/x7SpCD'}
-        ]
+        // store.js로 이동
+        // allUsers:[
+        //   {userId: 'hoza123', password: '123', name: 'Hoza', address: 'Seoul', src:'https://goo.gl/oqLfJR'},
+        //   {userId: 'max123', password: '456', name: 'Max', address: 'Berlin', src:'https://goo.gl/Ksk9B9'},
+        //   {userId: 'lego123', password: '789', name: 'Lego', address: 'Busan', src:'https://goo.gl/x7SpCD'}
+        // ]
+
       }
+    },
+    computed: {
+      ...mapGetters({
+        count: 'allUsersCount',
+        seouls: 'countOfSeoul',
+        percent: 'percentOfSeoul',
+      })
+      // ...mapGetters(['allUsersCount', 'countOfSeoul', 'percentOfSeoul']) // 불러와야 사용가능
+      ,
+      ...mapState(['allUsers'])
     },
     mounted() {
       EventBus.$on('signUp', users => {
-        this.allUsers.push(users)
+        this.$store.state.allUsers.push(users)
       })
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <h4>모든 유저의 수: {{ allUsersCount }}</h4>
   </v-container>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+      ...mapGetters(['allUsersCount', 'countOfSeoul', 'percentOfSeoul']) // 불러와야 사용가능
+    }
+}
+</script>
