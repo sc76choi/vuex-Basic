97124d61d2bf49893dfc0b1d27ef71dfaeadf592
dasom037 <dasom037@gmail.com>
2022-03-26 10:23:58 +0900

    Edit login success

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,19 @@
     <v-container fill-height style="max-width:450px;">
         <v-layout align-center row wrap>
             <v-flex xs12>
+                <v-alert
+                    class="mb-3"
+                    :value="isError"
+                    type="error"
+                    >
+                    아이디와 비밀번호를 입력해 주세요.
+                </v-alert>
+                <v-alert
+                    :value="loginSuccess"
+                    type="success"
+                    >
+                    로그인이 완료되었습니다.
+                </v-alert>
                 <v-card>
                     <v-toolbar flat height="50">
                         <v-toolbar-title>Login</v-toolbar-title>
@@ -16,6 +29,9 @@
                             type="password"
                             label="password를 입력하세요."
                         ></v-text-field>
+                        <v-btn color="primary" large block depressed
+                        @click="login"
+                        >로그인</v-btn>
                     </div>
                 </v-card>
             </v-flex>
@@ -28,7 +44,44 @@ export default {
     data() {
         return {
             email: null,
-            password: null
+            password: null,
+            allUsers: [
+                {id:1, name:'sc', email:'sc@gmail.com', password:'1234'},
+                {id:2, name:'uw', email:'uw@gmail.com', password:'1234'}
+            ],
+            isError: false,
+            loginSuccess: false
+        }
+    },
+    methods: {
+        login() {
+            // 전체 유저에서 해당 이메일로 유저를 찾는다.
+            let selectedUser = null
+            this.allUsers.forEach(user => {
+                if (user.email === this.email) selectedUser = user
+            })
+            
+            if (selectedUser === null ) {
+                this.isError = true
+                //alert('입력하신 이메일이 없습니다.')
+            } else {
+                console.log(selectedUser.password, this.password)
+
+                if(selectedUser.password !== this.password ) {
+                    this.isError = true
+                } else {
+                    this.isError = false
+                    this.loginSuccess = true
+                }
+                // if(selectedUser.password !== this.password) {
+                //     alert('입력하신 이메일과 비밀번호가 일치하지 않습니다.')
+                // } else {
+                //     alert('로그인이 완료되었습니다.')
+                // }
+            }
+            // 그 유저의 비밀번호화 입력된 비밀번호를 비교한다.
+            console.log(this.email, this.password)
+            return selectedUser
         }
     }
 }
